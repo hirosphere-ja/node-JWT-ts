1926ef93fb1e456c84b8a5ef07925464a140d70d
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-08-28 17:09:28 +0900

    ルーターを設定
    srcディレクトリ内にroutesディレクトリを作成
    routesディレクトリ内にuserRouter.tsを作成してルーティングを設定して動作確認

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from "express";
 import config from "./config/config";
+import userRouter from "./routes/userRouter";
 
 const app = express();
+
+app.use("/api/v1/user", userRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Hello JWT`);
 });
 
 app.listen(config.server.port, () => {
-  console.log(`Example app listening at http://localhost:${config.server.port}`);
+  console.log(
+    `Example app listening at http://localhost:${config.server.port}`
+  );
 });
