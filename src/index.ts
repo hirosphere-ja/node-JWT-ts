16aff4a19461069e601ec0a4e3dafa63a18fad53
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-08-28 14:46:12 +0900

    dotenvを適用
    srcディレクトリ内にconfigディレクトリを作成
    configディレクトリ内にconfig.tsを作成してサーバーとデータベースの環境変数を設定
    .envを作成

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
-const app = express();
+import config from "./config/config";
 
-const PORT = 3000;
+const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Hello JWT`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+app.listen(config.server.port, () => {
+  console.log(`Example app listening at http://localhost:${config.server.port}`);
 });
